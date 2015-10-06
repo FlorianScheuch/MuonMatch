305396947f80e18c30bab95e3700ce6777cb7efa
Florian <scheuch@physik.rwth-aachen.de>
2015-10-06 11:24:56 +0200

    Save

--- muonmatch_cfg.py
+++ muonmatch_cfg.py
@@ -8,6 +8,10 @@ process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAl
 process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorOpposite_cfi")
 process.load("RecoMuon.DetLayers.muonDetLayerGeometry_cfi")
 
+# configure modules via Global Tag
+# https://twiki.cern.ch/twiki/bin/view/CMS/SWGuideFrontierConditions
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+process.GlobalTag.globaltag = 'START53_V19::All'
 
 
 
@@ -19,19 +23,36 @@ process.MessageLogger = cms.Service("MessageLogger",
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 process.options = cms.untracked.PSet(SkipEvent = cms.untracked.vstring('ProductNotFound'))
 fileList = cms.untracked.vstring()
-fileList.extend(['/store/mc/Summer12_DR53X/ZZTo4mu_8TeV_mll8_mZZ95-160-powheg15-pythia6/AODSIM/PU_S10_START53_V19-v1/10000/0017F55A-EDF8-E211-B97E-003048C692DE.root'])
-
+#F2F1C38E-79DA-E111-B6E8-003048FFCB6A.root
+fileList.extend(['file:/user/scheuch/CMSSW/crab/CMSSW_5_3_12_patch2/src/MuonMatch/MuonMatch/F2F1C38E-79DA-E111-B6E8-003048FFCB6A.root'])
+#root://xrootd.unl.edu//store/mc/Summer12_DR53X/ZZTo4mu_8TeV_mll8_mZZ95-160-powheg15-pythia6/AODSIM/PU_S10_START53_V19-v1/10000/34675A33-99F9-E211-A1F4-0025907FD2BA.root
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = fileList
 )
 
+process.load("L1TriggerConfig.L1GtConfigProducers.L1GtTriggerMaskAlgoTrigConfig_cff")
+process.es_prefer_l1GtTriggerMaskAlgoTrig = cms.ESPrefer("L1GtTriggerMaskAlgoTrigTrivialProducer", "l1GtTriggerMaskAlgoTrig")
+
+process.load('HLTrigger.HLTfilters.hltLevel1GTSeed_cfi')
+process.hltLevel1GTSeed.L1TechTriggerSeeding = cms.bool(False)
+process.hltLevel1GTSeed.L1SeedsLogicalExpression = cms.string('L1_DoubleMu3')
+
 process.load('PhysicsTools.PatAlgos.mcMatchLayer0.muonMatch_cfi')
 process.muonMatch.src = cms.InputTag("muons")
+process.muonMatch.mcPdgId = cms.vint32(13)
 process.muonMatch.checkCharge = cms.bool(False)
 process.muonMatch.resolveAmbiguities = cms.bool(False)
-		
+process.muonMatch.maxDeltaR = cms.double(10.)
+process.muonMatch.maxDPtRel = cms.double(100.)
+process.muonMatch.resolveByMatchQuality = cms.bool(True)
+
+process.load("L1TriggerConfig.L1GtConfigProducers.l1GtTriggerMenuXml_cfi")
+process.l1GtTriggerMenuXml._errorstr = 0;
+
+process.load('L1Trigger.Skimmer.l1Filter_cfi')
+process.l1Filter.algorithms = cms.vstring('L1_DoubleMu3')
 
 #process.selectedMuonsGenParticlesMatchNew = cms.EDProducer("MCTruthDeltaRMatcherNew",
 #                                              src = cms.InputTag("muons"),
@@ -43,8 +64,11 @@ process.muonMatch.resolveAmbiguities = cms.bool(False)
 process.demo = cms.EDAnalyzer('MuonMatch'
 )
 
+#process.demo2 = cms.EDAnalyzer('MuonMatchPre'
+#)
+
 process.TFileService = cms.Service("TFileService",
     fileName = cms.string('AnalysisResult.root')
 )
 
-process.p = cms.Path(process.muonMatch*process.demo)
+process.p = cms.Path(process.muonMatch*process.demo) #process.l1GtTriggerMenuXml, process.l1Filter*  *process.hltLevel1GTSeed
